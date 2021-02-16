36fab0a982a1672dad9293bb06ac0f32e6c02dab
Minarose15 <76829050+Minarose15@users.noreply.github.com>
2021-02-16 12:18:10 -0700

    Finished and "classified"

--- Report week 6.py
+++ Report week 6.py
@@ -6,68 +6,70 @@ Created on Thu Feb 11 13:23:22 2021
 """
 
 from numpy import exp, linspace, sqrt, copy, pi, zeros_like, abs
-from numpy.fft import fft
 from matplotlib import pyplot as plt
 
-L = 1.0
-x0 = 0.4*L
-sigma = 0.1
-t = 0
-v = 3
-dt = 0.0099/v  #auto-adjusts to avoid r > 1
-runTime = 0.5
+class Wave:
+    
+    def __init__(self, L, sigma, speed):
+        self.L = L #1m
+        self.x0 = 0.4*self.L
+        self.sigma = sigma #0.1
+        self.v = speed
+        self.dt = 0.0099/self.v  #auto-adjusts to avoid r > 1
+        self.runTime = 0.5
 
-def Gauss(x):
-    return exp(-(x-x0)**2/(2*sigma**2))/(sigma*sqrt(2*pi))
+    #This function is to help with the plotting of the Gaussian wave
+    def Gauss(self, x):
+        return exp(-(x-self.x0)**2/(2*self.sigma**2))/(self.sigma*sqrt(2*pi))
+    
+    #this function creates the movie. It also keeps track of the point of the original peak over time
+    def propagateWave(self):
+        self.t = 0
+        steps = 100
+        x, dx = linspace(0, self.L, steps, retstep = True)
+        print(dx)
+        y = self.Gauss(x)
+        yNew = zeros_like(y)
+        yOld = copy(y)
+        con = self.v**2*self.dt**2/dx**2
+        self.f = []
+        lim = max(y)
 
-x, dx = linspace(0, L, 100, retstep = True)
-print(dx)
-y = Gauss(x)
-yNew = zeros_like(y)
-yOld = copy(y)
+        while(self.t<self.runTime):
+            yNew[1:-1] = 2*y[1:-1]-yOld[1:-1]+con*(y[2:]-2*y[1:-1]+y[:-2])
+            yNew[-1] = yNew[-2]
+            yOld = copy(y)
+            y = copy(yNew)
+            self.t += self.dt
+            plt.plot(x,y, "fuchsia")
+            ax = plt.axes()
+            ax.set_facecolor("k")
+            plt.ylim(-lim,lim)
+            plt.draw()
+            plt.pause(0.01)
+            plt.clf()
+            #this is for the Fourier analysis. This will be stored in the member variable for later. 
+            self.f.append(y[int(0.40*steps)-1])
 
-con = v**2*dt**2/dx**2
-print(con)
-plt.plot(x,y)
-plt.show()
+    #coding lesson's function, finds Fourier coefficients
+    def DFT(self, samples):
+        N = len(samples)
+        gamma = []
+        for k in range(N//2+1): 
+            gammaK = 0
+            for n,yn in enumerate(samples):
+                gammaK += yn * exp(-2j * pi * k * n/N )
+            gamma.append(gammaK/N)
 
-#ax = plt.axes()
-f = []
+        return gamma
 
-while(t<runTime):
-    yNew[1:-1] = 2*y[1:-1]-yOld[1:-1]+con*(y[2:]-2*y[1:-1]+y[:-2])
-    yNew[-1] = y[-2]
-    yOld = copy(y)
-    y = copy(yNew)
-    t += dt
-    plt.plot(x,y, "fuchsia")
-    plt.ylim(-4,4)
-    plt.draw()
-    plt.pause(0.01)
-    plt.clf()
-    f.append(y[39])
-     
-fS = 10000  #Rate at which I sample the function
-dt = 1/fS  # time between adjacent samples
-samplingTime = 1.0  # How long do I sample my function
-nSamples = int(samplingTime/dt)  # How many samples am I going to take
-
-def DFT(samples):
-    N = len(samples) #length of dataset
-    gamma = [] #create new array for all the coefficients
-    for k in range(N//2+1): #I'm not sure why we only go halfway. I feel like I remember something about it double 
-        #counting something
-        gammaK = 0
-        for n,yn in enumerate(samples):
-            gammaK += yn * exp(-2j * pi * k * n/N )
-        gamma.append(gammaK/N)
-
-    return gamma
-
-k = linspace(0,runTime//2,len(f)//2+1)
-#t = linspace(0,samplingTime,nSamples) 
-#f = Gauss(x)
-gamma = DFT(f)
-print(gamma)
-plt.plot(k, abs(gamma))
-plt.show()
+    #finds frequency spectrum of one single point over time on the string
+    def spectrumAnalysis(self):
+        k = linspace(0,1/(self.runTime*2),len(self.f)//2+1) #the 1/2*time is frequency/2
+        self.gamma = self.DFT(self.f)
+        plot2 = plt.plot(k, abs(self.gamma))
+        plt.show(plot2)
+        
+wave1 = Wave(1,0.1,3)
+wave1.propagateWave()
+wave1.spectrumAnalysis()
